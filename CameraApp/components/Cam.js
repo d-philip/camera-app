81b252c21bb4c3368656a658ecf05b5494fba9b0
d-philip <djphilip@bu.edu>
2021-04-30 00:38:18 -0400

    Added some barcode utility (barcode scanner not working though).

--- CameraApp/components/Cam.js
+++ CameraApp/components/Cam.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as FileSystem from 'expo-file-system';
 import * as Location from 'expo-location';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import { AuthContext } from "../App";
 import config from '../app.json';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
-import { IconButton, Colors } from 'react-native-paper';
+import { IconButton, Colors, Badge } from 'react-native-paper';
 import { v4 as uuidv4 } from 'uuid';
 
 const firebaseConfig = config.expo.web.config.firebase;
@@ -23,17 +24,32 @@ export default function Cam() {
   const { state, dispatch } = React.useContext(AuthContext);
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [hasLocationPermission, setHasLocationPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camType, setCamType] = useState(Camera.Constants.Type.back);
+  const [barType, setBarType] = useState(BarCodeScanner.Constants.Type.back);
   const [location, setLocation] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [scanData, setScanData] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   let camera: Camera;
 
   const handleCameraFlip = () => {
-    setType(
-      type === Camera.Constants.Type.back
+    setCamType(
+      camType === Camera.Constants.Type.back
         ? Camera.Constants.Type.front
         : Camera.Constants.Type.back
     );
+    setBarType(
+      barType === BarCodeScanner.Constants.Type.back
+        ? BarCodeScanner.Constants.Type.front
+        : BarCodeScanner.Constants.Type.back
+    );
+  };
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setScanData({type: type, data: data});
+    console.log('Scanned');
+    alert('Barcode detected.');
   };
 
   const handleCameraCapture = async () => {
@@ -42,13 +58,15 @@ export default function Cam() {
       const filename = FileSystem.documentDirectory + Date.now().toString() + '.jpg';
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
-
-      FileSystem.moveAsync({from: photo.uri, to: filename})
+      FileSystem.moveAsync({from: photo.uri, to: filename});
+      console.log(scanData);
       db.collection(state.email).doc(uuidv4()).set({
         filepath: filename,
         location: location,
         timestamp: Date.now().toString(),
+        barcode: scanned ? scanData : null,
       });
+      setScanned(false);
     }
   };
 
@@ -82,8 +100,13 @@ export default function Cam() {
 
   return (
       <View>
-        <Camera style={styles.camera} type={type} ref={(r) => { camera = r }}>
+
+        <Camera style={styles.camera} type={camType} ref={(r) => { camera = r }} >
+
           <View style={styles.buttonContainer}>
+            <Badge style={(scanned) ? styles.badgeOn : styles.badgeOff} >
+              {(scanned) ? 'Barcode Detected' : 'No Barcode Detected'}
+            </Badge>
             <IconButton
               icon="camera-switch"
               color={Colors.white}
@@ -98,6 +121,10 @@ export default function Cam() {
               size={40}
               onPress={() => handleCameraCapture()}
             />
+            {/*<BarCodeScanner
+              onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+              style={styles.barcode}
+            />*/}
           </View>
         </Camera>
       </View>
@@ -123,5 +150,23 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 18,
     color: 'white',
+  },
+  barcode: {
+    ...StyleSheet.absoluteFill,
+    backgroundColor: 'transparent',
+  },
+  badgeOn: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    backgroundColor: '#2a9d8f',
+    borderWidth: 0.75,
+  },
+  badgeOff: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    backgroundColor: '#ED771D',
+    borderWidth: 0.75,
   },
 });
